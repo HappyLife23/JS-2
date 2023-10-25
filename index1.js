7adbf93bbcf3def471970beefcc66201072ba4b2
David Heidari <davidheidari2@gmail.com>
2023-10-25 15:37:35 +0200

    made a DOM-folder

--- index1.js
+++ index1.js
@@ -7,7 +7,7 @@ Två steps:
 1. Declaration: var, let, const
 2. Assignment: = assignment operator
 
-*/
+
 
 let age = 21;
 let firstName = "David";
@@ -23,3 +23,67 @@ console.log("Enrolled:", student)
 document.getElementById("p1").innerHTML = "Hello " + firstName
 document.getElementById("p2").innerHTML = "You are " + age + " years old"
 document.getElementById("p3").innerHTML = "Enrolled: " + student
+
+*/
+/****** 
+ DOM-manipulation
+********/
+// övning 1
+const p = document.getElementById("myText");
+
+p.textContent = "Hello World"
+
+// övning 2
+
+
+/***** 
+ function
+ * ******/
+
+function greeting(){
+    console.log("Hello World 🌍")
+    
+}
+greeting();
+
+// övning 2 
+
+function number() {
+    console.log(3 *5)
+}
+number();
+
+// övning 3
+
+function convertToUpperCase(inputString) {
+    return inputString.toUpperCase();
+}
+
+// Anropa funktionen med en sträng som argument
+
+let originalString = "Hej världen!";
+let uperCaseString = convertToUpperCase(originalString);
+
+console.log(uperCaseString)
+
+//--------kör övningen igen 
+
+function convert(input) {
+    return input.toUpperCase();
+
+}
+let originaltext = "david";
+let changed = convert(originaltext);
+
+console.log(changed)
+
+//-------En gång till-------------
+
+function changing(userInput) {
+    return userInput.toUpperCase;
+}
+
+let mainText = "are you even trying??"
+let haveChanged = changing(mainText);
+
+console.log(have)
